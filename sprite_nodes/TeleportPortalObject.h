06e4c538af35ed02dddfbe8cc47c0961f78f9d84
ruslan831 <66568030+ruslan831@users.noreply.github.com>
2023-01-29 18:03:12 +0300

    Update TeleportPortalObject.h

--- sprite_nodes/TeleportPortalObject.h
+++ sprite_nodes/TeleportPortalObject.h
@@ -9,9 +9,9 @@ namespace gd {
     public:
         PAD(4); // i have no idea what this is, its not even set to 0 in the ctor
         TeleportPortalObject* m_pOrangePortal; // 0x46c
-        bool unk470;
-        float unk474;
-        bool unk478;
+        bool m_isYellowPortal;
+        float m_teleportYOffset;
+        bool m_teleportEase;
     };
 }
 
